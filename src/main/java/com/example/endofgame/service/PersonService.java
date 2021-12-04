a304d74da82870500ce744cb1c7d3665874809a7
lecaryna <64691773+lecaryna@users.noreply.github.com>
2021-12-04 00:33:58 -0800

    thread exercises

--- src/main/java/com/example/endofgame/service/PersonService.java
+++ src/main/java/com/example/endofgame/service/PersonService.java
@@ -1,10 +1,11 @@
 package com.example.endofgame.service;
-
 import com.example.endofgame.converter.PersonConverter;
 import com.example.endofgame.dto.PersonSummary;
 import com.example.endofgame.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutorService;
 
 @Service
 @Slf4j
@@ -14,13 +15,34 @@ public class PersonService {
 
     private final PersonConverter personConverter;
 
-    public PersonService(final PersonRepository repository, final PersonConverter personConverter) {
+    private final ExecutorService workers;
+
+    public PersonService(final PersonRepository repository, final PersonConverter personConverter,
+                         final ExecutorService workers) {
         this.repository = repository;
         this.personConverter = personConverter;
+        this.workers = workers;
     }
 
     public PersonSummary getMyPerson() {
         log.info("getMyPerson()");
         return personConverter.fromEntityToDto(repository.findMe());
+    }
+
+    public void runNewThread() {
+        Runnable anotherJob = new Runnable() {
+            @Override
+            public void run() {
+                log.info("inside anonymous class");
+                log.info("I'm running by:[" + Thread.currentThread().getName() + "]");
+            }
+        };
+        Runnable newJob = () -> {
+            log.info("inside lambda");
+            log.info("I'm running by:[" + Thread.currentThread().getName() + "]");
+        };
+
+        workers.submit(newJob);
+        workers.submit(anotherJob);
     }
 }
