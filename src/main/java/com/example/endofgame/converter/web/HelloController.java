60c05962187af20b3f6ed6d86a522cb7afbe4a36
lecaryna <64691773+lecaryna@users.noreply.github.com>
2021-12-13 10:16:15 -0800

    [add] - web controller and html added

---
+++ src/main/java/com/example/endofgame/converter/web/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.endofgame.converter.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@Slf4j
+public class HelloController {
+
+    //parameters are put after ? char
+    @GetMapping("/first")
+    public String hello(@RequestParam(value = "login", required = false) String login,
+                        @RequestParam(value = "password", defaultValue = "not-provided") String password,
+                        Model valuesForTemplate) {
+        log.info("login: [{}], password: [{}]", login, password);
+
+        valuesForTemplate.addAttribute("pass", password);
+        valuesForTemplate.addAttribute("user", login);
+
+        return "hello";
+    }
+}
